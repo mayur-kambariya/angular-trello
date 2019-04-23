80cbde9854183aaf8572ad24d921849102d59265
Mayur Kambariya <mayurkambariya@gmail.com>
2019-04-23 09:30:11 +0530

    initial commit angular trello

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
 import { Component } from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-drag-drop';
+    todoLists = [
+        {
+            id: 'todoList',
+            name: 'To Do',
+            cardList: [
+                'Learn New thing',
+                'SQL Database Issue',
+                'Create payment invoice',
+            ]
+        },
+        {
+            id: 'progressList',
+            name: 'Progress',
+            cardList: [
+                'Todo App for testing purpose',
+                'Angular 7 new feature'
+            ]
+        },
+        {
+            id: 'doneList',
+            name: 'Done',
+            cardList: [
+                'Client communication',
+                'Project configuration for Python',
+                'Angular material design',
+                'Bootstrap and flutter different'
+            ]
+        }
+    ]
+
+    drop(event: CdkDragDrop<string[]>) {
+        if (event.previousContainer === event.container) {
+            moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+        } else {
+            transferArrayItem(event.previousContainer.data,
+                event.container.data,
+                event.previousIndex,
+                event.currentIndex);
+        }
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    DragDropModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
